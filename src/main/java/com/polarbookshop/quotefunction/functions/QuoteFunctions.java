5c390a3c09e069ce3eee38a0d5a5106fcc032b43
jempitman <80392341+jempitman@users.noreply.github.com>
2023-05-02 14:00:13 +0200

    first commit

---
+++ src/main/java/com/polarbookshop/quotefunction/functions/QuoteFunctions.java
@@ -0,0 +1,44 @@
+package com.polarbookshop.quotefunction.functions;
+
+import com.polarbookshop.quotefunction.domain.Quote;
+import com.polarbookshop.quotefunction.domain.QuoteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Configuration
+public class QuoteFunctions {
+    public static final Logger log =
+            LoggerFactory.getLogger(QuoteFunctions.class);
+
+    @Bean
+    Supplier<Flux<Quote>> allQuotes(QuoteService quoteService){
+        return () -> {
+            log.info("Getting all quotes");
+            return quoteService.getAllQuotes()
+                    .delaySequence(Duration.ofSeconds(1));
+        };
+    }
+
+    @Bean
+    Supplier<Mono<Quote>> randomQuote(QuoteService quoteService) {
+        return () -> {
+            log.info("Getting random quote");
+            return quoteService.getRandomQuote();
+        };
+    }
+
+    @Bean
+    Consumer<Quote> logQuote() {
+        return quote -> log.info("Quote: '{}' by {}",
+                quote.content(), quote.author());
+    }
+
+}
